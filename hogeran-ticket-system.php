f4939b0f19c1b29c1333dadf001264a97c554080
Mikael Holgersson <mikael.git@hogeran.se>
2016-04-15 16:26:57 +0200

    lagt till main options i backend för det som ska synas på varje ticketsida

--- hogeran-ticket-system.php
+++ hogeran-ticket-system.php
@@ -23,9 +23,8 @@ function hogeran_ts_add_admin_page() {
 	//Generate Hogeran Ticket System Admin Page
 	add_menu_page( 'Hogeran Ticket System Options', 'Ticket System', 'manage_options', 'hogeran_ts', 'hogeran_ts_create_page', '', 110 );
 	
-	//Generate Sunset Admin Sub Pages
+	//Generate hogeran_ts Admin Sub Pages
 	add_submenu_page( 'hogeran_ts', 'Hogeran Ticket System Options', 'General', 'manage_options', 'hogeran_ts', 'hogeran_ts_create_page' );
-	
 	
 	
 }
@@ -33,4 +32,45 @@ add_action( 'admin_menu', 'hogeran_ts_add_admin_page' );
 
 function hogeran_ts_create_page() {
 	require_once( 'hogeran_ts-admin.php' );
+}
+
+
+
+
+
+
+//Activate custom settings
+	add_action( 'admin_init', 'hogeran_ts_custom_settings' );
+function hogeran_ts_custom_settings() {
+	register_setting( 'hogeran_ts-settings-group', 'company_name' );
+	register_setting( 'hogeran_ts-settings-group', 'page_header' );
+	register_setting( 'hogeran_ts-settings-group', 'description' );
+	
+	add_settings_section( 'hogeran_ts-main-options', 'Main Options', 'hogeran_ts_main_options', 'hogeran_ts');
+	
+	add_settings_field( 'hogeran_ts-name', 'Company Name', 'hogeran_ts_company_name', 'hogeran_ts', 'hogeran_ts-main-options');
+	add_settings_field( 'hogeran_ts-header', 'Header', 'hogeran_ts_header', 'hogeran_ts', 'hogeran_ts-main-options');
+	add_settings_field( 'hogeran_ts-description', 'Description', 'hogeran_ts_description', 'hogeran_ts', 'hogeran_ts-main-options');
+}
+function hogeran_ts_main_options() {
+	echo 'Customize your main Options';
+}
+function hogeran_ts_company_name() {
+	$company_name = esc_attr( get_option( 'company_name' ) );
+	echo '<input type="text" name="company_name" value="'.$company_name.'" placeholder="Company name" /><p class="description">Write your company name.</p>';
+}
+function hogeran_ts_header() {
+	$page_header = esc_attr( get_option( 'page_header' ) );
+	echo '<input type="text" name="page_header" value="'.$page_header.'" placeholder="Page Header" /><p class="description">Write the page header that win be shown on all ticket pages.</p>';
+}
+function hogeran_ts_description() {
+	$description = esc_attr( get_option( 'description' ) );
+	echo '<input type="text" name="description" value="'.$description.'" placeholder="Description" /><p class="description">Write something smart.</p>';
+}
+
+//Sanitization settings
+function hogeran_ts_sanitize_twitter_handler( $input ){
+	$output = sanitize_text_field( $input );
+	$output = str_replace('@', '', $output);
+	return $output;
 }
--- hogeran_ts-admin.php
+++ hogeran_ts-admin.php
@@ -1,1 +1,23 @@
 <h1>Hogeran Ticket System Options</h1>
+
+<?php settings_errors(); ?>
+<?php 
+	
+	$company_name = esc_attr( get_option( 'company_name' ) );
+	$page_header = esc_attr( get_option( 'page_header' ) );
+	$description = esc_attr( get_option( 'description' ) );
+	
+?>
+<div class="hogeran_ts-preview">
+	<div class="hogeran_ts">
+		<h1 class="hogeran_ts-company_name"><?php print $company_name; ?></h1>
+		<h2 class="hogeran_ts-page_header"><?php print $page_header; ?></h2>
+		<h2 class="hogeran_ts-description"><?php print $description; ?></h2>
+	</div>
+</div>
+
+<form method="post" action="options.php" class="hogeran_ts-general-form">
+	<?php settings_fields( 'hogeran_ts-settings-group' ); ?>
+	<?php do_settings_sections( 'hogeran_ts' ); ?>
+	<?php submit_button(); ?>
+</form>
